3408f54f1594985341eead62e288c3110345305f
nora1elsaey23 <121055893+nora1elsaey23@users.noreply.github.com>
2022-12-20 22:03:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+            int count;
+            for (int i = n1; i <= n2; i++)
+            {             count = 0;
+
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                        count++;
+                }
+                if (count == 2)
+                    Console.WriteLine($"{i}Prime");
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
